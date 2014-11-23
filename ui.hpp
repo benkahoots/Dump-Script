378ca3935bb79dd36d6526b4cc464cc3f9960ece
benkahoots <benkahoots101@gmail.com>
2014-11-24 10:21:15 +1100

    Create ui.hpp

---
+++ ui.hpp
@@ -0,0 +1,31 @@
+dialog/ui.hpp
+
+Add into: Class Background and Class Controls 
+
+	- //controls background
+				class toiletHIcon : life_RscPicture
+			{
+				
+				idc = -1;
+				text = "icons\toilet.paa";
+				x = safeZoneX+safeZoneW-0.11; y = safeZoneY+safeZoneH-0.40;
+				w = 0.02; h = 0.03;
+			};
+	- //class controls 
+				class shittextßs
+			{
+				type=0;
+				idc=23520;
+				style=0;
+				x=-1;
+				y=-1;
+				w=0.3;
+				h=0.05;
+				sizeEx=0.03;
+				size=1;
+				font="PuristaSemibold";
+				colorBackground[]={0,0,0,0};
+				colorText[] = { 1 , 1 , 1 , 1 };
+				shadow=true;
+				text="";
+			};
